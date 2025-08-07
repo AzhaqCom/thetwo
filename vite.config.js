13db38ef83bcc25b16c83224520f038c4728c7c5
arno <arnaudleg.dev@gmail.com>
2025-08-07 17:38:27 +0200

    add

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// Utilise la variable d'environnement, ou une chaîne vide par défaut en local
 const base_path = process.env.VITE_APP_BASE_PATH || '';
 
 export default defineConfig({
   plugins: [react()],
   base: base_path,
-});
+})
