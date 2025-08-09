e67b8315efd5f30f84f97249f7e21de69b75a7d5
AzhaqCom <126684194+AzhaqCom@users.noreply.github.com>
2025-08-09 18:02:58 +0200

    Fix useState import in CombatInitializer

--- src/components/combat/CombatInitializer.jsx
+++ src/components/combat/CombatInitializer.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { enemyTemplates } from '../../data/enemies';
 import { getModifier } from '../utils/utils';
 
